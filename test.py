2415d3bdb1e99d5ebb3ae0d476038ad8ebc24771
Scott Dosk <scott.dosk@gmail.com>
2024-02-25 18:28:08 -0500

    seperated out testing scripts

---
+++ test.py
@@ -0,0 +1,23 @@
+import rubix
+import cv2
+
+#open camera
+cap = cv2.VideoCapture(2)
+
+#Grab still image
+rubix.vid2still(cap)
+
+# path
+path = r'./still.png'
+
+#Reading an image in default mode
+image = cv2.imread(path)
+
+# prep image
+prepped_image = rubix.blur_sharpen(image)
+
+#identify interesting square regions
+squares = rubix.find_squares(prepped_image)
+
+for square in squares:
+    print(rubix.name_color(rubix.get_square_color(image, square)))
